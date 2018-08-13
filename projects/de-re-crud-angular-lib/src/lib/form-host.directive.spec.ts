6dade3d345634975a1f20752f792813019a35abf
Greg Beaty <gregbeatyjr@gmail.com>
2018-08-12 20:21:28 -0400

    Fix test

--- projects/de-re-crud-angular-lib/src/lib/form-host.directive.spec.ts
+++ projects/de-re-crud-angular-lib/src/lib/form-host.directive.spec.ts
@@ -2,7 +2,8 @@ import { FormHostDirective } from './form-host.directive';
 
 describe('FormHostDirective', () => {
   it('should create an instance', () => {
-    const directive = new FormHostDirective();
+    const viewContainerRef: any = {};
+    const directive = new FormHostDirective(viewContainerRef);
     expect(directive).toBeTruthy();
   });
 });
