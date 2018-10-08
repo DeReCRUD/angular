3ace8b5cc753c95ee9cd762448883665dfffe9ae
Greg Beaty <gregbeatyjr@gmail.com>
2018-10-08 18:14:04 -0400

    Fix the wrapNgComponent helper

--- projects/de-re-crud-angular-lib/src/lib/wrap-component.ts
+++ projects/de-re-crud-angular-lib/src/lib/wrap-component.ts
@@ -2,52 +2,86 @@ import {
   Component,
   Injector,
   ComponentFactoryResolver,
-  ComponentRef
+  ComponentRef,
+  NgZone,
+  ApplicationRef
 } from '@angular/core';
 import { wrapComponent } from '@de-re-crud/core';
 import { ComponentConstructor } from '@de-re-crud/core/models/constructors';
 import { IRenderer } from '@de-re-crud/core/models/renderers';
 
-interface IComponentCache {
-  [rendererId: string]: any;
-}
-
-const cache: IComponentCache = {};
-
 export interface NgComponentConstructor<P> {
   new (...params: any[]): P & Component;
 }
 
+class DynamicComponentLoader<TComponent> {
+  private appRef: ApplicationRef;
+  private componentFactoryResolver: ComponentFactoryResolver;
+  private zone: NgZone;
+  private componentRef: ComponentRef<TComponent>;
+  private element: Element;
+
+  constructor(
+    private injector: Injector,
+    private componentConstructor: NgComponentConstructor<TComponent>
+  ) {
+    this.appRef = injector.get(ApplicationRef);
+    this.zone = injector.get(NgZone);
+    this.componentFactoryResolver = injector.get(ComponentFactoryResolver);
+  }
+
+  loadComponentIfNecessary = (element: Element) => {
+    if (typeof this.componentRef !== 'undefined' && this.element === element) {
+      return;
+    }
+
+    this.zone.run(() => {
+      try {
+        const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+          this.componentConstructor
+        );
+
+        this.componentRef = componentFactory.create(this.injector, [], element);
+        this.element = element;
+        this.appRef.attachView(this.componentRef.hostView);
+      } catch (e) {
+        console.error(
+          'Unable to load component',
+          this.componentConstructor,
+          'at',
+          element
+        );
+        throw e;
+      }
+    });
+  };
+
+  refreshComponent = changes => {
+    Object.assign(this.componentRef.instance, changes);
+    this.componentRef.changeDetectorRef.detectChanges();
+  };
+
+  destroyComponent = () => {
+    if (this.componentRef) {
+      this.componentRef.destroy();
+    }
+  };
+}
+
 export function wrapNgComponent<TComponent extends IRenderer>(
-  factory: ComponentFactoryResolver,
   injector: Injector,
   ngComponent: NgComponentConstructor<TComponent>
 ): ComponentConstructor<TComponent> {
-  const componentFactoryResolver = factory.resolveComponentFactory(ngComponent);
+  const dynamicComponentLoader = new DynamicComponentLoader(
+    injector,
+    ngComponent
+  );
 
   return wrapComponent<TComponent>(
     (props: Readonly<TComponent>, nativeElement: Element) => {
-      let component: ComponentRef<TComponent> = cache[
-        props.rendererId
-      ] as ComponentRef<TComponent>;
-
-      if (!component) {
-        component = componentFactoryResolver.create(
-          injector,
-          [],
-          nativeElement
-        );
-
-        cache[props.rendererId] = component;
-      }
-
-      Object.assign(component.instance, props);
-      component.changeDetectorRef.detectChanges();
-
-      return () => {
-        delete cache[component.instance.rendererId];
-        component.destroy();
-      };
+      dynamicComponentLoader.loadComponentIfNecessary(nativeElement);
+      dynamicComponentLoader.refreshComponent(props);
+      return dynamicComponentLoader.destroyComponent;
     }
   );
 }
