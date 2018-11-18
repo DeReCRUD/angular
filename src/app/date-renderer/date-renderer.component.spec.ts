a653828191e6d828eff7a455b42f85afd70fd600
Greg Beaty <gregbeatyjr@gmail.com>
2018-11-18 12:03:03 -0500

    Fix lint issues

--- src/app/date-renderer/date-renderer.component.spec.ts
+++ src/app/date-renderer/date-renderer.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DateRendererComponent } from './date-renderer.component';
 
 describe('DateRendererComponent', () => {
@@ -8,9 +7,8 @@ describe('DateRendererComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DateRendererComponent ]
-    })
-    .compileComponents();
+      declarations: [DateRendererComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
